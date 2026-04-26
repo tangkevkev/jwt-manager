7dfdee8726caf5cfca75e29fd43d04a09ebe3567
kevin <kevin.tang@gmx.ch>
2026-04-26 19:06:02 +0200

    Add github actions to automatically deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/jwt-manager/',
   plugins: [
     tailwindcss(),
     VitePWA({
